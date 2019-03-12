880a06472a6f7d879eb487b835279e13ea7d5bc7
fatirishman53 <odin53@yahoo.com>
2019-03-12 02:06:55 +0000

    Updates

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var app = express();
 app.use(express.static(__dirname));
 app.use('/css', express.static(__dirname + '/css'));
 
-var server = app.listen(8080);
+var server = app.listen(8082, () => {
+  console.log("See http://grizzly-fatirishman53.c9users.io:8082...")
+});
